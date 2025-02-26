c67c5f63634f8f04fb13eabd681441bf24aa71b5
juankhusuma <juan.d.khusuma@gmail.com>
2025-02-27 04:33:40 +0700

    [GREEN] Implemented QnA Chatbot API and passed unit test

--- app/api/qna/route.ts
+++ app/api/qna/route.ts
@@ -1,3 +1,18 @@
+import { google } from '@ai-sdk/google';
+import { streamText } from 'ai';
+
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+
 export async function POST(req: Request) {
-    return;
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: google('gemini-2.0-flash-exp'),
+    messages,
+  });
+
+  return result.toDataStreamResponse();
 }
