9bcd3b0c4e393a5fe56ca19e71053a2f9c088790
Bryan Tjandra <bryan.tjandra@ui.ac.id>
2025-03-20 19:25:45 +0700

    [CHORES] add config for Markdownview testing

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: [
+      "@babel/preset-env",       // For ES6+ JavaScript
+      "@babel/preset-react",     // For React JSX
+      "@babel/preset-typescript" // If you're using TypeScript
+    ],
+  };
+  
